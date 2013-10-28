afbbf9fb3503af2bdebb4e56cad0649964d556fe
Daniel Liebeskind <dliebeskind@Daniels-MacBook-Pro-2.local>
2013-10-28 16:23:08 -0700

    initial commit adds key files

---
+++ server.js
@@ -0,0 +1,9 @@
+(function() {
+  var express, faye, path, drone;
+
+  express = require("express");
+  faye = require("faye");
+  path = require("path");
+  drone = require("ar-drone").createClient();
+
+})
