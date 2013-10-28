8a13b9107d4175fe4bb12a2e449341907be73efb
liebeskind <daniel.liebeskind@gmail.com>
2013-10-28 16:42:44 -0700

    Basic Node.js server created using express

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 (function() {
-  var express, faye, path, drone;
+  var express, faye, path, drone, server;
 
   express = require("express");
-  faye = require("faye");
   path = require("path");
-  drone = require("ar-drone").createClient();
+  app = express();
+  app.configure(function() {
+  	app.set('port', process.env.PORT || 3001);
+  	app.use(app.router);
+  });
 
-})
+  server = require('http').createServer(app);
+
+  server.listen(app.get('port'), function() {
+  	return console.log("Express server listening on port" + app.get("port"));
+  })
+
+}).call(this);
