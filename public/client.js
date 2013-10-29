02fc990ef73c40f3bea4ff21b501fd506e57289a
liebeskind <daniel.liebeskind@gmail.com>
2013-10-28 22:01:16 -0700

    Adds keydown values to client.js

---
+++ public/client.js
@@ -0,0 +1,34 @@
+
+
+keymap = {
+  87: { // w
+    action: 'front'
+  },
+  83: { // s
+    action: 'back'
+  },
+  65: { // a
+    action: 'left'
+  },
+  68: { // d
+    action: 'right'
+  },
+  38: { // up arrow
+    action: 'up'
+  },
+  40: { // down arrow
+    action: 'down'
+  },
+  37: {  // left arrow
+    action: 'counterClockwise'
+  },
+  39: { // right arrow
+    action: 'clockwise'
+  },
+  32: {  // spacebar
+    action: 'takeoff'
+  },
+  13: { // enter
+    action: 'land'
+  }
+};
