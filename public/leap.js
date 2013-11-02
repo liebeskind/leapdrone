7511c9acd2bb69553ca044705dfa1e5e3677ae11
liebeskind <daniel.liebeskind@gmail.com>
2013-11-01 21:29:56 -0700

    Flies using Leap!

--- public/leap.js
+++ public/leap.js
@@ -1,10 +1,6 @@
 (function() {
 
-//refactor to pseudoclassical
-
-  console.log("leap active");
-
-  var controller, circleCount, active, flying, ref, speed, leap, faye;
+  var controller, circleCount, active, flying, ref, speed, leap, faye, timeout;
 
   faye = new Faye.Client("/faye", {
     timeout: 60 // may need to adjust. If server doesn't send back any data for the given period of time, the client will assume the server has gone away and will attempt to reconnect. Timeout is given in seconds and should be larger than timeout on server side to give the server ample time to respond.
@@ -16,6 +12,7 @@
   ref = {};
   ref.fly = true;
   speed = 0.1;
+  timeout = 300;
 
    var main = function(frame) {
     if (!active) return;
@@ -59,50 +56,49 @@
        var zPos = pos[2];
 
        var adjX = xPos / 250;
-       console.log (adjX);
        var adjY = (yPos - 60) / 500;
        var adjZ = zPos / 200;
 
        if (adjX < 0 && ref.fly) {
-          console.log("left");
+          setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'left',
-      	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+      	   speed: speed // can refactor to control based on extent of finger movement 
+   			 })}, timeout);
        } else if (adjX > 0 && ref.fly) {
-        console.log("right");
+          setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'right',
       	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+   			 })}, timeout);
        }
 
        if (adjY > 0.5 && ref.fly) {
-        console.log("up");
+         setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'up',
       	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+         })}, timeout);
        } else if (adjY < 0.5 && ref.fly) {
-        console.log("down");
+        setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'down',
       	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+   			 })}, timeout);
        }
 
        if (adjZ < 0 && ref.fly) {
-        console.log("front");
+        setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'front',
       	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+   			 })}, timeout);
        } else if (adjZ > 0 && ref.fly) {
-        console.log("back");
+        setTimeout(function(){
          return faye.publish("/drone/move", {
       	   action: 'back',
       	   speed: speed // can refactor to control based on extent of finger movement
-   			 });
+   			 })}, timeout);
        }
      
      } else {
